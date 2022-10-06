bd59f21dd238a97abb81e1b94daf1bc536b772d8
FahadHosein <fahad.hosein@my.uwi.edu>
2022-10-06 16:29:16 -0400

    Cleaned app

--- build/newlib/src/termios.d
+++
@@ -1,9 +0,0 @@
-src/termios.o: \
- /home/fahad/esp/ESP8266_RTOS_SDK/components/newlib/src/termios.c \
- /home/fahad/esp/workspace/LAB1/build/include/sdkconfig.h \
- /home/fahad/esp/ESP8266_RTOS_SDK/components/newlib/platform_include/sys/termios.h
-
-/home/fahad/esp/workspace/LAB1/build/include/sdkconfig.h:
-
-/home/fahad/esp/ESP8266_RTOS_SDK/components/newlib/platform_include/sys/termios.h:
-/home/fahad/esp/ESP8266_RTOS_SDK/components/newlib/src/termios.c:
